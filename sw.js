124b7282605888acb9d9bdf997e2264a26d616cf
Sayaz06 <adha86614@gmail.com>
2025-11-18 12:57:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "nutrien-cache-v6";
-const urlsToCache = ["./index.html","./manifest.json","./icon.png"];
+const CACHE_NAME = "nutrien-cache-v7";
+const urlsToCache = ["./index.html","./manifest.json","./iconNutrien.png"];
 self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
 self.addEventListener("fetch", e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+
