61af017c5939ca92d27f37c4dc182abceee9c185
Sayaz06 <adha86614@gmail.com>
2025-11-18 13:07:04 +0800

    Update cache with additional icon files
    Added new icons to the cache list for the service worker.

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = "nutrien-cache-v7";
-const urlsToCache = ["./index.html","./manifest.json","./iconNutrien.png"];
+const CACHE_NAME = "nutrien-cache-v7"; // Atau v8, jika v7 masih bermasalah
+const urlsToCache = [
+    "./index.html",
+    "./manifest.json",
+    "./iconNutrien-192.png", // Tambah
+    "./iconNutrien-512.png"  // Tambah
+];
 self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
 self.addEventListener("fetch", e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
-
